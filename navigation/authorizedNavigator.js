37d16e345096e289ca040d8130fbb3a3c41e3442
AidanOakleyMM <oakleyaidan@gmail.com>
2020-05-23 16:13:46 -0400

    changed how the routers work so they can utilize redux

--- navigation/authorizedNavigator.js
+++ navigation/authorizedNavigator.js
@@ -1,11 +1,29 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import React, { Component } from "react";
 import Home from "../screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { connect } from "react-redux";
 
-const AuthorizedNavigator = createAppContainer(
-  createStackNavigator({
-    Home: { screen: Home },
-  })
-);
+const Stack = createStackNavigator();
 
-export default AuthorizedNavigator;
+class AuthorizedNavigator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+const mapStateToProps = () => {
+  return {};
+};
+
+export default connect(mapStateToProps)(AuthorizedNavigator);
--- navigation/unauthorizedNavigator.js
+++ navigation/unauthorizedNavigator.js
@@ -1,10 +1,29 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import React, { Component } from "react";
 import Unauthorized from "../screens/Unauthorized";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { connect } from "react-redux";
 
-const UnauthorizedNavigator = createAppContainer(
-  createStackNavigator({
-    Unauthorized: { screen: Unauthorized },
-  })
-);
-export default UnauthorizedNavigator;
+const Stack = createStackNavigator();
+
+class UnauthorizedNavigator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Unauthorized" component={Unauthorized} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+const mapStateToProps = () => {
+  return {};
+};
+
+export default connect(mapStateToProps)(UnauthorizedNavigator);
